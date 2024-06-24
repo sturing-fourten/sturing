798959300a413ab6006e5d6147199aebae0cd7f9
imkrmin <zum2n2@gmail.com>
2024-06-24 23:59:41 +0900

    Feat: TapBar 컴포넌트 UI 구현

---
+++ src/components/commons/TapBar.tsx
@@ -0,0 +1,15 @@
+export default function TapBar() {
+  return (
+    <div className="bg-white w-[375px] h-[48px] flex justify-between items-center px-4 py-3">
+      <button className="w-full flex justify-center items-center text-gray-700 text-4 font-medium tracking-[-0.48px] leading-[24px] focus:text-gray-1000 focus:font-semibold">
+        추천
+      </button>
+      <button className="w-full flex justify-center items-center text-gray-700 text-4 font-medium tracking-[-0.48px] leading-[24px] focus:text-gray-1000 focus:font-semibold">
+        검색
+      </button>
+      <button className="w-full flex justify-center items-center text-gray-700 text-4 font-medium tracking-[-0.48px] leading-[24px] focus:text-gray-1000 focus:font-semibold">
+        내 스터디
+      </button>
+    </div>
+  );
+}
