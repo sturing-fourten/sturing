3a9b07284510b3ced00568f35a8e0b842c851a9c
Hayden <hayden.dev.k@gmail.com>
2024-06-26 13:49:29 +0900

    Feat: 스터디 리스트 DoneTab 퍼블리싱

--- src/app/mystudy/@tabs/done/page.tsx
+++ src/app/mystudy/@tabs/done/page.tsx
@@ -1,3 +1,9 @@
+import StudyDoneCard from "@/components/commons/card/StudyDoneCard";
+
 export default function DoneTabPage() {
-  return <>DoneTabPage</>;
+  return (
+    <div className="flex flex-col gap-3 py-5">
+      <StudyDoneCard />
+    </div>
+  );
 }
