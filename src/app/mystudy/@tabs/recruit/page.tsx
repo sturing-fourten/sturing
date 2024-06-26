d743f0d8afc3c0e6bc2e82d3b487bd874c5ff482
Hayden <hayden.dev.k@gmail.com>
2024-06-26 13:41:11 +0900

    Feat: 스터디 리스트 RecruitTab 퍼블리싱

--- src/app/mystudy/@tabs/recruit/page.tsx
+++ src/app/mystudy/@tabs/recruit/page.tsx
@@ -1,3 +1,11 @@
+import StudyAcceptCard from "@/components/commons/card/StudyAcceptCard";
+
 export default function RecruitTabPage() {
-  return <>RecruitTabPage</>;
+  return (
+    <div className="flex flex-col gap-3 py-5">
+      <StudyAcceptCard status={"APPLIED"} />
+      <StudyAcceptCard status={"APPLIED_VIEW"} />
+      <StudyAcceptCard status={"ACCEPTED"} />
+    </div>
+  );
 }
