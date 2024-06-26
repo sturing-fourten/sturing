bffb57d5868c63437ea3ec12426417ee36bf973e
imkrmin <zum2n2@gmail.com>
2024-06-26 21:56:39 +0900

    Fix: rebase 후 에러 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import Button from "@/components/commons/Button";
 import CreateButton from "@/components/commons/CreateButton";
 import Gnb from "@/components/commons/Gnb";
 import SearchBar from "@/components/commons/SearchBar";
 import TapBar from "@/components/commons/TapBar";
+import Banner from "@/components/domains/home/Banner";
 
 import Contents from "@/components/domains/home/Contents";
 
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center w-full mb-[48px] mx-auto scroll-smooth">
+    <div className="flex flex-col items-center w-full mb-[48px] mx-auto scroll-smooth relative">
       <Gnb />
       <TapBar />
+      <Banner />
 
       <div className="flex w-full py-[32px] px-[16px]">
         <SearchBar />
