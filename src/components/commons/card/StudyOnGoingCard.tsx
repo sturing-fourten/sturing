9c5dc8a9b10de6e738f9312224fead6080e302ec
Hayden <hayden.dev.k@gmail.com>
2024-06-27 19:24:22 +0900

    Refactor: StudyDetailInfo import 수정

--- src/components/commons/card/StudyOnGoingCard.tsx
+++ src/components/commons/card/StudyOnGoingCard.tsx
@@ -1,31 +1,10 @@
-import Image from "next/image";
-import { teammate, schedule, checkSquare } from "@/../public/icons/icons";
+import { StudyDetailInfo } from "./element/StudyDetailInfo";
 import StudyMeetingInfo from "./element/StudyMeetingInfo";
 
 interface IStudyOnGoingCardProps {
   isStarted: boolean;
 }
 
-function StudyDetailInfo() {
-  return (
-    <section className="flex justify-evenly items-center p-2 rounded-[3px] bg-white text-gray-1000 text-[14px] font-medium tracking-[-0.42px]">
-      <div className="flex gap-1">
-        <Image src={teammate} alt="teammate icon" width={18} height={18} />
-        {"팀원 4명"}
-      </div>
-      <span className="w-[1px] h-3 bg-gray-400"></span>
-      <div className="flex gap-1">
-        <Image src={schedule} alt="teammate icon" width={18} height={18} />
-        {"매주 토요일"}
-      </div>
-      <span className="w-[1px] h-3 bg-gray-400"></span>
-      <div className="flex gap-1">
-        <Image src={checkSquare} alt="teammate icon" width={18} height={18} />
-        {"사진 인증"}
-      </div>
-    </section>
-  );
-}
 export default function StudyOnGoingCard({ isStarted }: IStudyOnGoingCardProps) {
   return (
     <article
