c3cca8b3bb1a27812499d2d963b81d9e62c3a117
Hayden <hayden.dev.k@gmail.com>
2024-06-27 19:17:39 +0900

    Refactor: InsideMenu 공통 컴포넌트화

--- src/app/mystudy/@tabs/page.tsx
+++ src/app/mystudy/@tabs/page.tsx
@@ -1,18 +1,6 @@
 import StudyOnGoingCard from "@/components/commons/card/StudyOnGoingCard";
+import { InsideMenu } from "@/components/commons/card/element/InsideMenu";
 
-function InsideMenu(props: any) {
-  const { title, number, isCurrent } = props;
-  return (
-    <button
-      className={`
-    inline-flex justify-start items-center gap-0.5 px-4 py-2 rounded border text-gray-900
-    ${isCurrent ? "bg-main-100 border-main-300" : "bg-white border-main-200"}
-    `}>
-      <span className="text-gray-900 text-sm font-medium leading-tight">{title}</span>
-      <span className="text-gray-900 text-sm font-medium leading-tight">{number}</span>
-    </button>
-  );
-}
 export default function ProcessTabPage() {
   const studyList = [
     { id: 1, isStarted: true },
