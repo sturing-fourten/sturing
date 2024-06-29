f2b02ef0dd1ebf6496230db11be35bf81fabba6e
Hayden <hayden.dev.k@gmail.com>
2024-06-29 19:12:01 +0900

    Fix: StudyInfo UI 수정

--- src/components/domains/dashboard/StudyInfo.tsx
+++ src/components/domains/dashboard/StudyInfo.tsx
@@ -12,12 +12,10 @@ export default function StudyInfo() {
           <TagBase className="bg-main-100 text-gray-700 border-transparent">오프라인</TagBase>
           <TagBase className="bg-main-100 text-gray-700 border-transparent">디자인</TagBase>
         </div>
-        <div className="flex items-center text-gray-700 text-[12px] font-medium tracking-[-0.36px]">
+        <div className="flex items-center text-gray-400 text-[12px] font-medium tracking-[-0.36px]">
           {"온라인"}
           <span className="w-[1px] h-3 mx-2 bg-gray-400"></span>
           {"06.01~08.01"}
-          <span className="w-[1px] h-3 mx-2 bg-gray-400"></span>
-          {"ZOOM"}
         </div>
       </div>
 
