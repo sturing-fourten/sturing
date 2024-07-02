c85afd5405326ce540c1c6275273b94401d7fa3c
Yoon-kyungLee <yk2588@nate.com>
2024-07-03 04:19:17 +0900

    Feat: recruit 관련 상수 추가

---
+++ src/constant/progressWay.ts
@@ -0,0 +1,12 @@
+export const PROGRESSWAY_LIST: { [key: string]: string } = {
+  online: "온라인",
+  offline: "오프라인",
+};
+
+export const ONLINE_LIST: { [key: string]: string } = {
+  zoom: "ZOOM",
+  zep: "ZEP",
+  openTalk: "카카오 오픈톡",
+  discord: "디스코드",
+  slack: "슬랙",
+};
