72065c32a6127296088ac198632d7c352acd1253
Hayden <hayden.dev.k@gmail.com>
2024-07-02 18:40:11 +0900

    Fix: EmojiTag 커서 변경

--- src/components/domains/dashboard/EmojiTag.tsx
+++ src/components/domains/dashboard/EmojiTag.tsx
@@ -8,7 +8,7 @@ export default function EmojiTag(props: IEmojiTagProps) {
 
   return (
     <span
-      className={`flex justify-center items-center gap-1 w-[38px] h-[22px] rounded-3xl border text-xs cursor-pointer ${
+      className={`flex justify-center items-center gap-1 w-[38px] h-[22px] rounded-3xl border text-xs ${
         isIncludingMe ? "border-main-500 bg-main-100 text-gray-1000" : "border-gray-200 bg-gray-100 text-gray-300"
       }`}>
       <span>{emoji}</span>
