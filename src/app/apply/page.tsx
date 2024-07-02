bda17792756f5d0a14e42a17f9008e608d807c53
imkrmin <zum2n2@gmail.com>
2024-07-02 13:38:01 +0900

    Refator: Apply 페이지 리팩토링

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,3 +1,13 @@
+import WriteStep from "@/components/domains/apply/steps/WriteStep";
+import RoleStep from "@/components/domains/apply/steps/RoleStep";
+import SuccessStep from "@/components/domains/apply/steps/SucessStep";
+
 export default function ApplyPage() {
-  return null;
+  return (
+    <>
+      <WriteStep />
+      {/* <RoleStep /> */}
+      {/* <SuccessStep /> */}
+    </>
+  );
 }
