1e68ab209fb38c6a080f856db495a1071b17945b
Hayden <hayden.dev.k@gmail.com>
2024-07-03 18:58:00 +0900

    Design: 댓글, 답글 입력창 디자인 QA 적용

--- src/components/domains/dashboard/post/PostNestedCommentForm.tsx
+++ src/components/domains/dashboard/post/PostNestedCommentForm.tsx
@@ -6,16 +6,16 @@ export default function PostNestedCommentForm() {
   return (
     <form className="grid grid-cols-[25px_1fr] items-center gap-2" action="">
       <Avatar width={25} height={25} profileImageUrl={"https://picsum.photos/200/30"} hasBorder={true} />
-      <div className="grid grid-cols-[1fr_auto] gap-[5px] items-center h-7">
+      <div className="relative">
         <input
           type="text"
           name="nestedComment"
           placeholder="답글을 입력해주세요."
-          className="h-full pl-4 pr-[10px] border border-gray-300 rounded-[80px] text-xs font-medium leading-none placeholder:text-gray-600 focus:border-main-600 outline-none"
+          className="w-full h-[30px] pl-4 pr-[10px] border border-gray-300 rounded-[80px] text-xs font-medium leading-none placeholder:text-gray-600 focus:border-main-600 outline-none"
         />
 
-        <button type="submit" className="flex justify-center items-center">
-          <Image src={ICONS.sendComment.src} alt={ICONS.sendComment.alt} width={20} height={20} />
+        <button type="submit" className="absolute inset-y-0 right-[10px]">
+          <Image src={ICONS.sendComment.src} alt={ICONS.sendComment.alt} width={16} height={16} />
         </button>
       </div>
     </form>
