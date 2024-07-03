93bb1b84c4781639b13a2872584050adad407914
imkrmin <zum2n2@gmail.com>
2024-07-03 17:19:08 +0900

    Feat: Matching 페이지 LevelStep 컴포넌트 UI 구현

---
+++ src/app/matching/page.tsx
@@ -0,0 +1,19 @@
+import InterestStep from "@/components/domains/matching/steps/InterestStep";
+import LevelStep from "@/components/domains/matching/steps/LevelStep";
+import LocationStep from "@/components/domains/matching/steps/LocationStep";
+import MoodStep from "@/components/domains/matching/steps/MoodStep";
+import SuccessStep from "@/components/domains/matching/steps/SuccessStep";
+import TypeStep from "@/components/domains/matching/steps/TypeStep";
+
+export default function MatchingPage() {
+  return (
+    <>
+      <InterestStep />
+      {/* <LevelStep /> */}
+      {/* <LocationStep /> */}
+      {/* <MoodStep /> */}
+      {/* <TypeStep /> */}
+      {/* <SuccessStep /> */}
+    </>
+  );
+}
