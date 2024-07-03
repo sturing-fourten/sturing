0fafbfd8406ee856bcbfe9a1cf4a3fd5c1ebf485
Hayden <hayden.dev.k@gmail.com>
2024-07-03 10:27:33 +0900

    Feat: 댓글 텍스트 컴포넌트 퍼블리싱

---
+++ src/components/domains/dashboard/post/CommentContent.tsx
@@ -0,0 +1,3 @@
+export default function CommentContent({ content }: { content: string }) {
+  return <p className="text-black text-sm font-normal leading-snug">{content}</p>;
+}
---
+++ src/components/domains/dashboard/post/CommentItem.tsx
@@ -0,0 +1,24 @@
+import { TComment } from "@/types/board";
+import CommentHeader from "./CommentHeader";
+import CommentContent from "./CommentContent";
+
+interface ICommentItemProps {
+  isQualified: boolean;
+  comment: TComment;
+}
+
+export default function CommentItem(props: ICommentItemProps) {
+  const { isQualified, comment } = props;
+
+  return (
+    <div className="grid grid-cols-[38px_1fr] grid-rows-[auto_auto_auto] gap-x-2 gap-y-1">
+      <CommentHeader user={comment.user} created_at={comment.created_at} type={"comment"} />
+
+      <span></span>
+
+      {isQualified ? <CommentContent content={comment.content} /> : <div> 더미 이미지</div>}
+
+      <span></span>
+    </div>
+  );
+}
