9573d92fb205918dccc8c4381230a9982c3ec358
Hayden <82023300+HaydenDevK@users.noreply.github.com>
2024-07-03 16:45:49 +0900

    Feat: 관심목록 페이지 퍼블리싱 (#20)
    * Feat: 관심 목록 페이지, 강의 탭 퍼블리싱
    
    * Feat: 스터디 관심목록 탭 퍼블리싱
    
    * Fix: LectureCard 정렬 수정

---
+++ src/app/mypage/scrap/layout.tsx
@@ -0,0 +1,35 @@
+import TabBarLinkUnderlined from "@/components/commons/TabBarLinkUnderlined";
+import TopBar from "@/components/commons/TopBar";
+import { TTabMenuLinkUnderlinedItem } from "@/types/study";
+
+interface IDashboardProps {
+  params: {
+    id: string;
+  };
+  tabs: React.ReactNode;
+}
+
+export default function ScrapLayout({ params, tabs }: IDashboardProps) {
+  const { id } = params;
+
+  const hrefBase = `/mypage/scrap/`;
+
+  const SCRAP_TAB_MENU_LIST: TTabMenuLinkUnderlinedItem[] = [
+    { id: "study", title: "스터디", href: `${hrefBase}` },
+    { id: "lecture", title: "강의", href: `${hrefBase}lecture` },
+  ];
+
+  return (
+    <>
+      {/* Header */}
+      {/* TODO 공통 레이아웃 처리 */}
+      <TopBar variant="back">관심 목록</TopBar>
+
+      {/* Tab Menu */}
+      <TabBarLinkUnderlined defaultSegment={hrefBase} tabMenuList={SCRAP_TAB_MENU_LIST} />
+
+      {/* Tab Panel */}
+      {tabs}
+    </>
+  );
+}
