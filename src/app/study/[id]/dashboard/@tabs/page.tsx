3cbcd42ad8a22cc0a3682aa548ef7d0a741acfbd
Hayden <hayden.dev.k@gmail.com>
2024-07-03 18:48:31 +0900

    Refactor: 팀 탭 데이터 유무에 따른 분기 처리 개선

--- src/app/study/[id]/dashboard/@tabs/page.tsx
+++ src/app/study/[id]/dashboard/@tabs/page.tsx
@@ -8,55 +8,58 @@ import FunctionCardConnector from "@/components/domains/dashboard/FunctionCardCo
 
 export default function TeamTab() {
   const data = {
-    progress: null, // "진척도"
+    progress: {}, // "진척도"
     attendance: null, // "출석체크"
-    checkList: [], // "체크리스트"
+    checkList: [{}], // "체크리스트"
     proofList: [], // "사진 인증"
   };
 
-  const isAnyFeatureActive = data.progress || data.attendance || data.checkList.length > 0 || data.proofList.length > 0;
+  const isProgressExist = data.progress === null;
+  const isAttendanceExist = data.attendance === null;
+  const isCheckListExist = data.checkList.length > 0;
+  const isProofListExist = data.proofList.length > 0;
 
-  const isProgressActive = data.progress;
-  const isAttendanceActive = data.attendance;
-  const isCheckListActive = data.checkList.length > 0;
-  const isProofListActive = data.proofList.length > 0;
+  const isAnyFeatureExist = isProgressExist || isAttendanceExist || isCheckListExist || isProofListExist;
+  const isAnyFeatureNotExist = !isProgressExist || !isAttendanceExist || !isCheckListExist || !isProofListExist;
 
-  const progressHasNext = isAttendanceActive || isCheckListActive || isProofListActive;
-  const attendanceHasNext = isCheckListActive || isProofListActive;
-  const checkListActive = isProofListActive;
+  const progressHasNext = isAttendanceExist || isCheckListExist || isProofListExist;
+  const attendanceHasNext = isCheckListExist || isProofListExist;
+  const checkListExist = isProofListExist;
 
   return (
     <section className="flex flex-col py-7 px-4">
-      {isAnyFeatureActive && <StudyFunctionEditButton />}
+      {isAnyFeatureExist && <StudyFunctionEditButton />}
 
       <div className="flex flex-col gap-4">
-        {isProgressActive && (
+        {isProgressExist && (
           <div className="relative">
             <StudyMemberProgressCard />
             {progressHasNext && <FunctionCardConnector />}
           </div>
         )}
-        {isAttendanceActive && (
+        {isAttendanceExist && (
           <div className="relative">
             <StudyMemberAttendanceCard />
             {attendanceHasNext && <FunctionCardConnector />}
           </div>
         )}
-        {isAttendanceActive && (
+        {isCheckListExist && (
           <div className="relative">
             <StudyMemberChecklistCard />
-            {checkListActive && <FunctionCardConnector />}
+            {checkListExist && <FunctionCardConnector />}
           </div>
         )}
-        {isProofListActive && <StudyPhotoProof />}
+        {isProofListExist && <StudyPhotoProof />}
       </div>
 
-      <div className="flex flex-col gap-4">
-        {!isProgressActive && <StudyAddFunctionCard title="진척도" />}
-        {!isAttendanceActive && <StudyAddFunctionCard title="출석체크" />}
-        {!isCheckListActive && <StudyAddFunctionCard title="체크리스트" />}
-        {!isProofListActive && <StudyAddFunctionCard title="사진 인증" />}
-      </div>
+      {isAnyFeatureNotExist && (
+        <div className="flex flex-col gap-4 mt-4">
+          {!isProgressExist && <StudyAddFunctionCard title="진척도" />}
+          {!isAttendanceExist && <StudyAddFunctionCard title="출석체크" />}
+          {!isCheckListExist && <StudyAddFunctionCard title="체크리스트" />}
+          {!isProofListExist && <StudyAddFunctionCard title="사진 인증" />}
+        </div>
+      )}
     </section>
   );
 }
