ba85c5792bf5b27eadef8e3d07b669cbadbffe3d
Hayden <hayden.dev.k@gmail.com>
2024-07-03 11:33:00 +0900

    Feat: 스터디 관심목록 탭 퍼블리싱

--- src/app/mypage/scrap/@tabs/page.tsx
+++ src/app/mypage/scrap/@tabs/page.tsx
@@ -1,7 +1,14 @@
+import { StudyRecruitCard } from "@/components/commons/card/StudyRecruitCard";
+import Link from "next/link";
+
 export default function ScrapStudyPage() {
   return (
-    <>
-      <h1>StudyPage Component</h1>
-    </>
+    <ul className="grid grid-cols-2 gap-2 py-5 px-4">
+      {[1, 2, 3].map((lecture, index) => (
+        <Link href="" key={index}>
+          <StudyRecruitCard isMini={true} isScraped={true} />
+        </Link>
+      ))}
+    </ul>
   );
 }
