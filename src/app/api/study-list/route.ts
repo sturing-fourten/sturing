4de18a12af4911a103f3da66fe57bfe54e283610
Hayden <hayden.dev.k@gmail.com>
2024-07-07 15:42:05 +0900

    Refactor: studyList GET try catch 추가

--- src/app/api/study-list/route.ts
+++ src/app/api/study-list/route.ts
@@ -3,13 +3,19 @@ import { Study } from "@/schema/studySchema";
 
 export async function GET(request: Request) {
   connectDB();
-  const studyList = await Study.find({});
 
-  return Response.json(studyList);
+  try {
+    const studyList = await Study.find({});
+
+    return Response.json(studyList);
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
 
 export async function POST(request: Request) {
   connectDB();
+
   try {
     const newStudy = await request.json();
     if (!newStudy) throw new Error("스터디 정보가 없습니다.");
