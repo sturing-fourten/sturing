fb39cfb66d4ab60e10620b164597a5fb01154c06
Hayden <hayden.dev.k@gmail.com>
2024-07-10 16:06:53 +0900

    Refactor: 불필요한 코드 제거

--- src/app/api/apply/route.ts
+++ src/app/api/apply/route.ts
@@ -8,19 +8,17 @@ export async function PATCH(request: Request) {
   try {
     const { studyId, newApplication, newTeamMember } = await request.json();
 
-    const studyObjectId = studyId;
-
-    if (!studyObjectId || !newApplication || !newTeamMember) throw new Error("지원서 필수 정보가 없습니다.");
+    if (!studyId || !newApplication || !newTeamMember) throw new Error("지원서 필수 정보가 없습니다.");
 
     // 1) 지원서 생성
-    newApplication.studyId = studyObjectId;
+    newApplication.studyId = studyId;
     const { _id: applicationId } = await Application.create(newApplication);
     if (!applicationId) throw new Error("지원서 정보가 없습니다.");
 
     // 2) studyId로 teamMembers 조회해서 members에 추가 (지원서 id도 함께)
     newTeamMember.applicationId = applicationId;
 
-    const updateResult = await TeamMembers.updateOne({ studyId: studyObjectId }, { $push: { members: newTeamMember } });
+    const updateResult = await TeamMembers.updateOne({ studyId: studyId }, { $push: { members: newTeamMember } });
     if (updateResult.matchedCount === 0) throw new Error("해당하는 팀 멤버 리스트가 없습니다.");
     return Response.json("지원서 생성 성공");
   } catch (error: any) {
