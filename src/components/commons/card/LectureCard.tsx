6aad3de97c5035cc71289ee74a335c62eefe86d4
imkrmin <zum2n2@gmail.com>
2024-07-10 17:53:12 +0900

    Fix: lecture category 수정

--- src/components/commons/card/LectureCard.tsx
+++ src/components/commons/card/LectureCard.tsx
@@ -5,6 +5,7 @@ import { TagLight } from "../tag/TagLight";
 import { TagRate } from "../tag/TagRate";
 import Link from "next/link";
 import { LectureData } from "@/app/lecture/[id]/page";
+import { CATEGORY } from "@/constant/category";
 
 interface LectureCardProps {
   variant: "card" | "info";
@@ -14,23 +15,24 @@ interface LectureCardProps {
 
 export default function LectureCard({ isScraped, variant, lecture }: LectureCardProps) {
   const isCard = variant === "card";
+  const { online, category, platform, rating, title, instructor } = lecture;
 
   return (
     <Link href={`/lecture/${lecture._id}`}>
       <article className={isCard ? "py-[13px] px-[17px] border border-gray-300 rounded-[5px] bg-white" : ""}>
         <section className="flex justify-between items-center mb-3">
           <div className="flex items-center gap-1">
-            <TagMain>{lecture.online ? "온라인" : "오프라인"}</TagMain>
-            <TagLight>{lecture.category}</TagLight>
-            <TagLight>{lecture.platform}</TagLight>
-            <TagRate>{lecture.rating}</TagRate>
+            <TagMain>{online ? "온라인" : "오프라인"}</TagMain>
+            <TagLight>{CATEGORY(category)}</TagLight>
+            <TagLight>{platform}</TagLight>
+            <TagRate>{rating}</TagRate>
           </div>
           {isCard && (
             <Image src={isScraped ? bookmarkGrayOn : bookmarkGrayOff} alt="bookmark icon" width={24} height={24} />
           )}
         </section>
-        <p className="mb-1 text-[14px] font-semibold tracking-[-0.28px] text-black">{lecture.title}</p>
-        <p className="text-[12px] font-medium tracking-[-0.36px] text-gray-600">{lecture.instructor}</p>
+        <p className="mb-1 text-[14px] font-semibold tracking-[-0.28px] text-black">{title}</p>
+        <p className="text-[12px] font-medium tracking-[-0.36px] text-gray-600">{instructor}</p>
       </article>
     </Link>
   );
