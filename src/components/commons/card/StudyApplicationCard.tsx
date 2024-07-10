4e4a52a28ba11a90d1f3d5bbd95819612db8ba62
Hayden <hayden.dev.k@gmail.com>
2024-07-10 13:40:56 +0900

    Feat: 지원서 리스트 페이지 연결

--- src/components/commons/card/StudyApplicationCard.tsx
+++ src/components/commons/card/StudyApplicationCard.tsx
@@ -1,28 +1,31 @@
 import StudyApplyInfo from "./element/StudyApplyInfo";
-import StudyCardButton from "./element/StudyCardButton";
 import Avatar from "../Avatar";
+import { TApplicationSummary } from "@/types/application";
+import StudyCardLink from "./element/StudyCardLink";
 
-export default function StudyApplicationCard({ status }: { status: "APPLIED" | "APPLIED_VIEW" | "ACCEPTED" }) {
+export default function StudyApplicationCard(props: TApplicationSummary) {
+  const { _id: applicationId, profileImageUrl, status, nickname, title } = props;
+  status;
   return (
     <article className="flex flex-col gap-4 px-5 py-6 bg-white border border-gray-300 rounded-lg">
       <StudyApplyInfo status={status} />
 
       <section className="flex items-center justify-stretch gap-3">
-        <Avatar width={40} height={40} profileImageUrl={"https://picsum.photos/200/30"} hasBorder={true} />
+        <Avatar width={40} height={40} profileImageUrl={profileImageUrl} hasBorder={true} />
 
         <div>
           <div className="flex items-center gap-2 text-[12px] font-medium tracking-[-0.36px]">
-            <span className="text-gray-700">{"피그마마스터"}</span>
+            <span className="text-gray-700">{nickname}</span>
             <span className="w-[1px] h-3 bg-gray-400"></span>
-            <span className="text-gray-600">{"비기너"}</span>
+            <span className="text-gray-600">{"직업"}</span>
+            <span className="w-[1px] h-3 bg-gray-400"></span>
+            <span className="text-gray-600">{"직업 수준"}</span>
           </div>
-          <p className="text-[12px] font-semibold tracking-[-0.36px] text-gray-900">
-            {"안녕하세요 쏘카 실무 마스터 스터디 지원합니다."}
-          </p>
+          <p className="text-[12px] font-semibold tracking-[-0.36px] text-gray-900">{title}</p>
         </div>
       </section>
 
-      <StudyCardButton>지원서 보기</StudyCardButton>
+      <StudyCardLink href="/">지원서 보기</StudyCardLink>
     </article>
   );
 }
--- src/types/application.ts
+++ src/types/application.ts
@@ -5,4 +5,10 @@ export type TApplication = {
   title: string;
   resolution: string;
   role: "팀장" | "부팀장" | "과제팀장" | "출결팀장" | "기록팀장" | "일정팀장" | "팀원";
+  nickname: string;
+  profileImageUrl: string;
+  status: "APPLIED" | "APPLIED_VIEW" | "ACCEPTED";
 };
+
+export type TApplicationSummary = Omit<TApplication, "resolution" | "role">;
+export type TApplicationList = TApplicationSummary[];
