abc7d088a383e9458636bc140a6a303ebbd8eb3e
Joohyun Kim <trust987@naver.com>
2024-07-11 22:55:38 +0900

    Refactor: 강의 상세보기 api 리턴값에 관련 스터디 리스트 추가

--- src/app/api/lecture/[id]/route.ts
+++ src/app/api/lecture/[id]/route.ts
@@ -1,16 +1,21 @@
 import connectDB from "@/lib/database/db";
 import { Lecture } from "@/schema/lectureSchema";
+import { Study } from "@/schema/studySchema";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(req: Request, { params }: { params: { id: string } }) {
   const id = params.id;
 
   try {
     await connectDB();
     const lecture = await Lecture.findById(id);
-    return Response.json({ lecture });
+    if (!lecture) {
+      return Response.json({ message: "해당 강의가 존재하지 않습니다." }, { status: 404 });
+    }
+
+    const relatedStudyList = await Study.find({ lectureId: id, status: "RECRUIT_START" });
+
+    return Response.json({ lecture, relatedStudyList });
   } catch (error: any) {
-    return new Response(JSON.stringify({ error: "강의를 불러오는데 실패하였습니다." }), {
-      status: 500,
-    });
+    return Response.json({ message: "강의를 불러오는데 실패하였습니다." }, { status: 500 });
   }
 }
