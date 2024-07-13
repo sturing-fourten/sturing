8e887776f8e30d2060c12015972b9e04dafd0c06
Joohyun Kim <trust987@naver.com>
2024-07-13 17:30:16 +0900

    Feat: 강의 리스트 검색 필터 api 구현

--- src/app/api/lecture/route.ts
+++ src/app/api/lecture/route.ts
@@ -1,11 +1,56 @@
 import connectDB from "@/lib/database/db";
-import { Lecture } from "@/schema/lectureSchema";
+import { Lecture, LectureType } from "@/schema/lectureSchema";
+import categoryMap from "@/utils/categoryMap";
 
-export async function GET(req: Request) {
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const category = searchParams.get("category")?.split(",");
+  const search = searchParams.get("search");
+
+  await connectDB();
+  let query: { [key: string]: any } = {};
+
+  if (category && category.length > 0) {
+    query.category = { $in: category };
+  }
+
+  if (search) {
+    const mappedCategory = categoryMap[search];
+    query.$or = [
+      { title: { $regex: search, $options: "i" } },
+      { category: mappedCategory || { $regex: search, $options: "i" } },
+    ];
+  }
+
   try {
-    await connectDB();
-    const lectures = await Lecture.find({});
-    return Response.json({ lectures });
+    const lectureListData = await Lecture.find(query);
+
+    const lectureList: {
+      id: string;
+      online: boolean;
+      category: string;
+      platform: string;
+      rating: number;
+      title: string;
+      instructor: string;
+    }[] = [];
+
+    if (lectureListData) {
+      for (const lecture of lectureListData) {
+        lectureList.push({
+          id: lecture._id.toString(),
+          online: lecture.online,
+          category: lecture.category,
+          platform: lecture.platform,
+          rating: lecture.rating,
+          title: lecture.title,
+          instructor: lecture.instructor,
+        });
+      }
+    }
+    //북마크 여부  Promise.all 로 추가
+
+    return Response.json({ lectureList });
   } catch (error: any) {
     console.error("Error fetching lectures:", error);
     return new Response(JSON.stringify({ error: "강의 리스트를 불러오는데 실패하였습니다." }), {
