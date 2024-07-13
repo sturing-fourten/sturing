bec0bdd09f3d073c5e57d05993991e11b9516b20
Hayden <hayden.dev.k@gmail.com>
2024-07-13 13:32:53 +0900

    Feat: 미팅 일정 형태로 문자열 포맷하는 함수 구현

---
+++ src/utils/getDayTimeByDate.ts
@@ -0,0 +1,17 @@
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
+
+export function getDayTimeByDate(date: Date): [string, string] {
+  const dayOfWeek = format(date, "EEE", { locale: ko });
+
+  const formattedDate = format(date, "MM.dd", { locale: ko });
+
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const isAM = hours < 12;
+  const formattedHours = hours % 12 || 12;
+  const period = isAM ? "오전" : "오후";
+  const formattedTime = `${period} ${formattedHours}:${minutes.toString().padStart(2, "0")}`;
+
+  return [`${formattedDate}(${dayOfWeek})`, formattedTime];
+}
