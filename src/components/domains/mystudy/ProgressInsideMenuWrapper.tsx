6bbfb4a72097e73f666c977e04406694cf1a6f9f
Hayden <hayden.dev.k@gmail.com>
2024-07-14 18:56:25 +0900

    Feat: 현재 리스트 타입 상태 추가 및 연동

--- src/components/domains/mystudy/ProgressInsideMenuWrapper.tsx
+++ src/components/domains/mystudy/ProgressInsideMenuWrapper.tsx
@@ -3,12 +3,13 @@ import { InsideMenu } from "../../commons/card/element/InsideMenu";
 import { useMyStudyListStore } from "@/store/myStudyListStore";
 
 export function ProgressInsideMenuWrapper() {
+  const currentListType = useMyStudyListStore.getState().currentListType;
   const progressStudyListCount = useMyStudyListStore.getState().progressStudyListCount;
   const recruitEndStudyListCount = useMyStudyListStore.getState().recruitEndStudyListCount;
   return (
     <nav className="flex gap-3 mb-4">
       <form action={fetchProgressStudyListAction}>
-        <InsideMenu title="진행 중" number={progressStudyListCount} isCurrent={true} />
+        <InsideMenu title="진행 중" number={progressStudyListCount} isCurrent={currentListType === "PROGRESS"} />
       </form>
       <form action={""}>
         <InsideMenu title="진행 예정" number={recruitEndStudyListCount} isCurrent={false} />
--- src/store/myStudyListStore.ts
+++ src/store/myStudyListStore.ts
@@ -2,6 +2,10 @@ import { create } from "zustand";
 import { TStudy } from "@/types/study";
 
 interface IMyStudyListState {
+  currentListType: "PROGRESS" | "RECRUIT_END" | "RECRUIT_START_OWNER" | "RECRUIT_START_MEMBER" | "DONE";
+  setCurrentListType: (
+    newType: "PROGRESS" | "RECRUIT_END" | "RECRUIT_START_OWNER" | "RECRUIT_START_MEMBER" | "DONE",
+  ) => void;
   currentStudyList: TStudy[] | null;
   setCurrentStudyList: (newList: TStudy[]) => void;
   progressStudyListCount: number;
@@ -11,6 +15,12 @@ interface IMyStudyListState {
 }
 
 export const useMyStudyListStore = create<IMyStudyListState>((set) => ({
+  currentListType: "PROGRESS",
+  setCurrentListType: (
+    newType: "PROGRESS" | "RECRUIT_END" | "RECRUIT_START_OWNER" | "RECRUIT_START_MEMBER" | "DONE",
+  ) => {
+    set({ currentListType: newType });
+  },
   currentStudyList: null,
   setCurrentStudyList: (newList: TStudy[]) => {
     set({ currentStudyList: newList });
