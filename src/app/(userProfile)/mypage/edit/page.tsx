b81f67ca8f45842286c14a495bab4fc7d1d39690
Yoon-kyungLee <yk2588@nate.com>
2024-07-14 22:55:05 +0900

    Feat: 이미지 변경 기능 추가-진행중

--- src/app/(userProfile)/mypage/edit/page.tsx
+++ src/app/(userProfile)/mypage/edit/page.tsx
@@ -15,7 +15,6 @@ export default function MypageEdit() {
   const { user, fetchUser } = useUserStore();
   const { matching, fetchMatching } = useMatchingStore();
   const [image, setImage] = useState<string>("");
-  const [fileToRead, setFileToRead] = useState<File | null>(null);
 
   const [name, setName] = useState<string>("");
   const [nickname, setNickname] = useState<string>("");
@@ -25,30 +24,6 @@ export default function MypageEdit() {
   const [ageIsVisible, setAgeIsVisible] = useState<boolean>(false);
   const [genderIsVisible, setGenderIsVisible] = useState<boolean>(false);
   const [locationIsVisible, setLocationIsVisible] = useState<boolean>(false);
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    if (!files) return;
-    const file = files[0];
-    if (file?.size > 1 * 1024 * 1024) {
-      alert("파일 크기는 1MB를 초과할 수 없습니다.");
-      return;
-    }
-    setFileToRead(file);
-  };
-
-  useEffect(() => {
-    if (!fileToRead) return;
-
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      const newImage = reader.result as string;
-      setImage(newImage);
-    };
-
-    reader.readAsDataURL(fileToRead);
-    setFileToRead(null);
-  }, [fileToRead, setImage]);
 
   useEffect(() => {
     if (!user) {
@@ -69,7 +44,7 @@ export default function MypageEdit() {
   }, [user, matching]);
 
   return (
-    <form action={editProfile}>
+    <form className="w-full" action={editProfile}>
       <div className="w-full flex-col inline-flex">
         <div>
           <TopBar variant="edit">프로필 수정</TopBar>
@@ -77,7 +52,7 @@ export default function MypageEdit() {
         </div>
         <div className="flex-1 w-full pt-6 pb-10 flex-col inline-flex justify-start items-center">
           <div className="pb-5">
-            <ProfileImageUpload handleFileChange={handleFileChange} image={image} />
+            <ProfileImageUpload image={image} setImage={setImage} />
           </div>
           <div className="w-full flex-col inline-flex justify-start items-start gap-6 px-4">
             <Title>기본정보</Title>
--- src/components/domains/mypageEdit/ProfileImageUpload.tsx
+++ src/components/domains/mypageEdit/ProfileImageUpload.tsx
@@ -2,37 +2,85 @@
 
 import Avatar from "@/components/commons/Avatar";
 import { ICONS } from "@/constant/icons";
-import { useRef } from "react";
+import { PutBlobResult } from "@vercel/blob";
+import { SetStateAction, useEffect, useRef, useState } from "react";
 
 interface ProfileImageUploadProps {
-  handleFileChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   image: string;
+  setImage: React.Dispatch<SetStateAction<string>>;
 }
 
 export default function ProfileImageUpload(props: ProfileImageUploadProps) {
-  const { handleFileChange, image } = props;
+  const { image, setImage } = props;
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [blob, setBlob] = useState<PutBlobResult | null>(null);
 
-  const handleUploadClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
+  const onUploadImage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!fileInputRef.current?.files) {
+      throw new Error("No file selected");
     }
+
+    const file = fileInputRef.current.files[0];
+
+    const response = await fetch(`/api/image-test?filename=${file.name}`, {
+      method: "POST",
+      body: file,
+    });
+
+    console.log(file, response);
+
+    const newBlob = (await response.json()) as PutBlobResult;
+
+    setBlob(newBlob);
+    setImage(blob?.url || image);
   };
 
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!files) return;
+    const file = files[0];
+    if (file?.size > 1 * 1024 * 1024) {
+      alert("파일 크기는 1MB를 초과할 수 없습니다.");
+      return;
+    }
+    setImage(blob?.url || image);
+  };
+
+  useEffect(() => {
+    if (!blob) return;
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImage(blob.url);
+    };
+  }, [image, blob?.url]);
+
+  console.log(blob?.url);
+  console.log(image);
+
   return (
-    <div className="w-fit relative">
-      <button type="button" onClick={handleUploadClick}>
-        <input hidden name="profileImageUrl" value={image} onChange={handleFileChange} />
-        <input
-          type="file"
-          ref={fileInputRef}
-          accept="image/*"
-          onChange={handleFileChange}
-          style={{ display: "none" }}
-        />
-        <Avatar profileImageUrl={image} width={90} height={90} />
-        <img className="w-6 h-6 absolute right-0 bottom-0" src={ICONS.cameraCircle.src} alt={ICONS.cameraCircle.alt} />
-      </button>
-    </div>
+    <form onSubmit={onUploadImage}>
+      <div className="w-fit relative">
+        <button type="submit" onClick={() => fileInputRef.current?.click()}>
+          <input hidden name="profileImageUrl" value={image} onChange={handleFileChange} />
+          <input
+            name="file"
+            type="file"
+            ref={fileInputRef}
+            accept="image/*"
+            onChange={handleFileChange}
+            style={{ display: "none" }}
+          />
+          <Avatar profileImageUrl={image} width={90} height={90} />
+          <img
+            className="w-6 h-6 absolute right-0 bottom-0"
+            src={ICONS.cameraCircle.src}
+            alt={ICONS.cameraCircle.alt}
+          />
+        </button>
+      </div>
+    </form>
   );
 }
