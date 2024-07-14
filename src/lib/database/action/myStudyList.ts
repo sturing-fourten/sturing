b4afb1f0030745a0c217f741558015316cdefdea
Hayden <hayden.dev.k@gmail.com>
2024-07-14 18:59:40 +0900

    Feat: 진행 예정 내 스터디 리스트 fetch action 구현

--- src/lib/database/action/myStudyList.ts
+++ src/lib/database/action/myStudyList.ts
@@ -28,3 +28,26 @@ export const fetchProgressStudyListAction: TMyStudyListAction = async () => {
     revalidatePath("/mystudy");
   } catch (error) {}
 };
+
+export const fetchRecruitEndStudyListAction: TMyStudyListAction = async () => {
+  useMyStudyListStore.getState().setCurrentListType("RECRUIT_END");
+
+  const session = await getSession();
+  const userId = session?.user?.id;
+
+  if (!userId) throw new Error("유저 정보가 필요합니다.");
+
+  try {
+    const url = `${process.env.LOCAL_URL}/api/my-study/list?userId=${userId}&listType=RECRUIT_END`;
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("스터디 목록을 불러오는 데 실패했습니다.");
+
+    const { recruitEndStudyList, progressStudyListCount } = await response.json();
+    if (!recruitEndStudyList || !progressStudyListCount) throw new Error("스터디 목록을 불러오는 데 실패했습니다.");
+
+    useMyStudyListStore.getState().setCurrentStudyList(recruitEndStudyList);
+    useMyStudyListStore.getState().setRecruitEndStudyListCount(recruitEndStudyList.length);
+    useMyStudyListStore.getState().setProgressStudyListCount(progressStudyListCount);
+    revalidatePath("/mystudy");
+  } catch (error) {}
+};
