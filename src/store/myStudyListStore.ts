0f08991b2654205633b9f283989775bdacbdddb6
Hayden <hayden.dev.k@gmail.com>
2024-07-14 18:02:14 +0900

    Refactor: 내 스터디 리스트 스토어 수정

--- src/store/myStudyListStore.ts
+++ src/store/myStudyListStore.ts
@@ -1,16 +1,32 @@
-import { getSession } from "@/lib/database/getSession";
-import { TStudy } from "@/types/study";
-import { revalidatePath } from "next/cache";
 import { create } from "zustand";
+import { TStudy } from "@/types/study";
 
 interface IMyStudyListState {
-  myStudyList: TStudy[] | null;
-  setMyStudyList: (newMyStudyList: TStudy[]) => void;
+  progressStudyList: TStudy[] | null;
+  setProgressStudyList: (newList: TStudy[]) => void;
+  progressStudyListCount: number;
+  setProgressStudyListCount: (newCounts: number) => void;
+  recruitEndStudyList: TStudy[] | null;
+  setRecruitEndStudyList: (newList: TStudy[]) => void;
+  recruitEndStudyListCount: number;
+  setRecruitEndStudyListCount: (newCounts: number) => void;
 }
 
 export const useMyStudyListStore = create<IMyStudyListState>((set) => ({
-  myStudyList: null,
-  setMyStudyList: (newMyStudyList: TStudy[]) => {
-    set({ myStudyList: newMyStudyList });
+  progressStudyList: null,
+  setProgressStudyList: (newList: TStudy[]) => {
+    set({ progressStudyList: newList });
+  },
+  progressStudyListCount: 0,
+  setProgressStudyListCount: (newCounts: number) => {
+    set({ progressStudyListCount: newCounts });
+  },
+  recruitEndStudyList: null,
+  setRecruitEndStudyList: (newList: TStudy[]) => {
+    set({ recruitEndStudyList: newList });
+  },
+  recruitEndStudyListCount: 0,
+  setRecruitEndStudyListCount: (newCounts: number) => {
+    set({ recruitEndStudyListCount: newCounts });
   },
 }));
