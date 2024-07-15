e391dcb81149ae0d0c2dbb2a73f20e43d8f90d48
Hayden <hayden.dev.k@gmail.com>
2024-07-15 14:28:07 +0900

    Refactor: 내 스터디 리스트 GET API switch 문 코드 정리

--- src/app/api/my-study/list/route.ts
+++ src/app/api/my-study/list/route.ts
@@ -13,51 +13,53 @@ export async function GET(request: Request) {
       /*
        * PROGRESS, RECRUIT_END, DOME : 내가 참여하고 있는 모든 스터디 (개설 or 지원 후 ACCEPTED) 중 각 status로 필터링한 목록 조회
        */
-      case "PROGRESS":
-        const progressTeamMembers = await TeamMembers.find({
+      case "PROGRESS": {
+        const teamMembers = await TeamMembers.find({
           "members.userId": userId,
           $or: [{ "members.isOwner": true }, { "members.status": "ACCEPTED" }],
         });
-        const progressStudyIdList = progressTeamMembers.map((member) => member.studyId);
-        const progressStudyList = await Study.find({
-          _id: { $in: progressStudyIdList },
+        const studyIdList = teamMembers.map((member) => member.studyId);
+        const studyList = await Study.find({
+          _id: { $in: studyIdList },
           status: listType,
         }).populate({
           path: "teamMembersId",
           select: "members",
         });
         return Response.json({
-          progressStudyList,
+          progressStudyList: studyList,
           recruitEndStudyListCount: await Study.countDocuments({
-            _id: { $in: progressStudyIdList },
+            _id: { $in: studyIdList },
             status: "RECRUIT_END",
           }),
         });
+      }
 
-      case "RECRUIT_END":
-        const recruitEndTeamMembers = await TeamMembers.find({
+      case "RECRUIT_END": {
+        const teamMembers = await TeamMembers.find({
           "members.userId": userId,
           $or: [{ "members.isOwner": true }, { "members.status": "ACCEPTED" }],
         });
-        const recruitEndStudyIdList = recruitEndTeamMembers.map((member) => member.studyId);
-        const recruitEndStudyList = await Study.find({
-          _id: { $in: recruitEndStudyIdList },
+        const studyIdList = teamMembers.map((member) => member.studyId);
+        const studyList = await Study.find({
+          _id: { $in: studyIdList },
           status: listType,
         }).populate({
           path: "teamMembersId",
           select: "members",
         });
         return Response.json({
-          recruitEndStudyList,
+          recruitEndStudyList: studyList,
           progressStudyListCount: await Study.countDocuments({
-            _id: { $in: recruitEndStudyIdList },
+            _id: { $in: studyIdList },
             status: "PROGRESS",
           }),
         });
+      }
 
-      case "RECRUIT_START_OWNER":
+      case "RECRUIT_START_OWNER": {
         // 내가 개설한 모든 스터디 중 status가 RECRUIT_START인 목록 조회
-        const recruitStartOwnerStudyList = await Study.find({
+        const studyList = await Study.find({
           ownerId: userId,
           status: "RECRUIT_START",
         }).populate({
@@ -65,22 +67,22 @@ export async function GET(request: Request) {
           select: "members",
         });
 
-        const recruitStartMemberTeamMembers = await TeamMembers.find(
+        const teamMembers = await TeamMembers.find(
           {
             "members.userId": userId,
           },
           { "members.isOwner": false },
         );
-        const recruitStartMemberStudyIdList = recruitStartMemberTeamMembers.map((member) => member.studyId);
-        const recruitStartMemberStudyListCount = await Study.countDocuments({
-          _id: { $in: recruitStartMemberStudyIdList },
-          status: "RECRUIT_START",
-        });
+        const studyIdList = teamMembers.map((member) => member.studyId);
 
         return Response.json({
-          recruitStartOwnerStudyList,
-          recruitStartMemberStudyListCount,
+          recruitStartOwnerStudyList: studyList,
+          recruitStartMemberStudyListCount: await Study.countDocuments({
+            _id: { $in: studyIdList },
+            status: "RECRUIT_START",
+          }),
         });
+      }
 
       // case "RECRUIT_START_MEMBER":
       //   // 내가 지원한 모든 스터디 중 RECRUIT_START 상태인 목록 조회
@@ -92,14 +94,14 @@ export async function GET(request: Request) {
       //   // const recruitStartStudyIds = myRecruitStartTeamMembers.map((member) => member.studyId);
       //   // studyList = await Study.find({ _id: { $in: recruitStartStudyIds }, status: "RECRUIT_START" });
       //   break;
-      case "DONE":
-        const doneTeamMembers = await TeamMembers.find({
+      case "DONE": {
+        const teamMembers = await TeamMembers.find({
           "members.userId": userId,
           $or: [{ "members.isOwner": true }, { "members.status": "ACCEPTED" }],
         });
-        const doneStudyIdList = doneTeamMembers.map((member) => member.studyId);
-        const doneStudyList = await Study.find({
-          _id: { $in: doneStudyIdList },
+        const studyIdList = teamMembers.map((member) => member.studyId);
+        const studyList = await Study.find({
+          _id: { $in: studyIdList },
           status: listType,
         }).populate({
           path: "teamMembersId",
@@ -111,7 +113,8 @@ export async function GET(request: Request) {
           },
         });
 
-        return Response.json(doneStudyList);
+        return Response.json(studyList);
+      }
 
       default:
         throw new Error("listType이 잘못되었습니다.");
