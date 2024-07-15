5cda7aca650f327d060b0cdf50c2af48af486414
Yoon-kyungLee <yk2588@nate.com>
2024-07-15 23:54:01 +0900

    Fix: FixedBottomBar 프롭 명명관련 오류 해결

--- src/components/domains/detail/FixedBottomBar.tsx
+++ src/components/domains/detail/FixedBottomBar.tsx
@@ -2,14 +2,14 @@ import UserActionButton from "@/components/domains/detail/UserActionButton";
 import ScrapButton from "@/components/commons/ScrapButton";
 interface FixedBottomBarProps {
   page: "lecture" | "study";
-  lectureId?: string;
+  id: string;
 }
 
 export default function FixedBottomBar({ page, id }: FixedBottomBarProps) {
   return (
     <>
       <footer className="sm:absolute fixed inset-x-0 bottom-0 flex items-center justify-between gap-1 h-[74px] bg-white px-4 py-3 drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]">
-        <ScrapButton variant="detail" lectureId={lectureId || ""} />
+        <ScrapButton variant="detail" page={page} id={id || ""} />
         <UserActionButton page={page} />
       </footer>
     </>
