5e0c0159ed9c0331016ad522b2e55f899ca89eeb
Hayden <hayden.dev.k@gmail.com>
2024-07-17 15:13:58 +0900

    Design: NoList UI 수정

--- src/components/domains/mystudy/NoList.tsx
+++ src/components/domains/mystudy/NoList.tsx
@@ -1,6 +1,6 @@
 export default function NoList({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex items-center justify-center py-6 px-5 rounded-lg border border-gray-300 text-gray-700 text-[12px] font-semibold tracking-[-0.36px]">
+    <div className="flex items-center justify-center py-6 px-5 rounded-lg text-gray-700 text-[12px] font-semibold tracking-[-0.36px]">
       {children}
     </div>
   );
