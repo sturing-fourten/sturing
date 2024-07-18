51f9af748b55719b89e87e0156dbc1066a6501aa
이윤경 <148832727+Yoon-kyungLee@users.noreply.github.com>
2024-07-18 16:21:22 +0900

    Fix: 프로필 이미지 수정 시 제대로 주소를 가져오지 않는 버그 해결 (#77)

--- src/app/api/userProfile/route.ts
+++ src/app/api/userProfile/route.ts
@@ -1,6 +1,8 @@
 import connectDB from "@/lib/database/db";
 import { getSession } from "@/lib/database/getSession";
 import { User } from "@/schema/userSchema";
+import { del, put } from "@vercel/blob";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   await connectDB();
@@ -24,4 +26,25 @@ export async function GET(request: Request) {
     console.error("사용자 조회 중 오류:", error);
     return new Response(JSON.stringify({ error: "내부 서버 오류" }), { status: 500 });
   }
+}
+
+export async function POST(request: Request): Promise<NextResponse> {
+  const { searchParams } = new URL(request.url);
+  const filename = searchParams.get("filename");
+
+  if (filename && request.body) {
+    const blob = await put(filename, request.body, {
+      access: "public",
+    });
+
+    return NextResponse.json(blob);
+  } else {
+    return NextResponse.json({ error: "No filename or body" }, { status: 400 });
+  }
+}
+
+export async function DELETE(request: Request): Promise<NextResponse> {
+  const { url } = await request.json();
+  await del(url);
+  return NextResponse.json({});
 }
