c4867230f9153f738266c24601152d49be5fcb14
Hayden <hayden.dev.k@gmail.com>
2024-07-19 19:57:59 +0900

    Feat: 지원서 리스트 API에 levels 추가 및 UI 연동

--- src/components/commons/card/StudyApplicationCard.tsx
+++ src/components/commons/card/StudyApplicationCard.tsx
@@ -2,9 +2,14 @@ import StudyApplyInfo from "./element/StudyApplyInfo";
 import Avatar from "../Avatar";
 import { TApplicationSummary } from "@/types/application";
 import StudyCardLink from "./element/StudyCardLink";
+import { getFirstInterestLevel } from "@/utils/getFirstInterestLevel";
+import { CATEGORY } from "@/constant/category";
+import { CAREER_LIST } from "@/constant/teamMemberInfo";
 
 export default function StudyApplicationCard(props: TApplicationSummary) {
-  const { profileImageUrl, status, nickname, title } = props;
+  const { profileImageUrl, status, nickname, title, levels } = props;
+  const { interest, level } = getFirstInterestLevel(levels.levels);
+
   return (
     <article className="flex flex-col gap-4 px-5 py-6 bg-white border border-gray-300 rounded-lg">
       <StudyApplyInfo status={status} />
@@ -16,9 +21,9 @@ export default function StudyApplicationCard(props: TApplicationSummary) {
           <div className="flex items-center gap-2 text-[12px] font-medium tracking-[-0.36px]">
             <span className="text-gray-700">{nickname}</span>
             <span className="w-[1px] h-3 bg-gray-400"></span>
-            <span className="text-gray-600">{"직업"}</span>
+            <span className="text-gray-600">{CATEGORY(interest)}</span>
             <span className="w-[1px] h-3 bg-gray-400"></span>
-            <span className="text-gray-600">{"직업 수준"}</span>
+            <span className="text-gray-600">{CAREER_LIST[level]}</span>
           </div>
           <p className="text-[12px] font-semibold tracking-[-0.36px] text-gray-900">{title}</p>
         </div>
