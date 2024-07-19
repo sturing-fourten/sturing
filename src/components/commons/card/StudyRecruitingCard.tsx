3ef04b561e61276817c3651e257c190d44c2a636
Hayden <hayden.dev.k@gmail.com>
2024-07-19 22:01:44 +0900

    Feat: 스터디 카드 클릭 시 대시보드 페이지로 이동

--- src/components/commons/card/StudyRecruitingCard.tsx
+++ src/components/commons/card/StudyRecruitingCard.tsx
@@ -3,6 +3,7 @@ import StudyCardLink from "./element/StudyCardLink";
 import { StudyDetailInfo } from "./element/StudyDetailInfo";
 import StudyMeetingInfo from "./element/StudyMeetingInfo";
 import { TMyStudy, TTeamMembersIdAddedMember } from "@/types/study";
+import Link from "next/link";
 
 interface IStudyRecruitingCardProps {
   study: TMyStudy;
@@ -11,7 +12,7 @@ interface IStudyRecruitingCardProps {
 export default function StudyRecruitingCard(props: IStudyRecruitingCardProps) {
   const {
     study: {
-      _id,
+      _id: studyId,
       title,
       startDate,
       endDate,
@@ -27,16 +28,16 @@ export default function StudyRecruitingCard(props: IStudyRecruitingCardProps) {
 
   const dateRange = getDateRange(startDate, endDate);
   const where = (format === "ONLINE" ? platform : location) ?? "";
-  const meetingDay = day === "추후협의" ? day : `매주 ${day}`;
+  const meetingDay = `매주 ${day}`;
   const memberCount = (teamMembersId as TTeamMembersIdAddedMember)?.members?.length;
 
   return (
-    <article className="py-6 px-5 border border-gray-300 bg-white rounded-lg">
+    <Link className="py-6 px-5 border border-gray-300 bg-white rounded-lg" href={`/study/${studyId}/dashboard`}>
       <StudyMeetingInfo format={"ONLINE" ? "온라인" : "오프라인"} dateRange={dateRange} where={where} />
       <p className="mt-2 text-gray-1000 text-[16px] font-semibold tracking-[-0.32px]">{title}</p>
       <hr className="my-4" />
       <StudyDetailInfo className="mb-4" meetingDay={meetingDay} task={"과제 없음"} memberCount={memberCount} />
-      <StudyCardLink href={`/application-list/${_id}`}>지원서 리스트 보기</StudyCardLink>
-    </article>
+      <StudyCardLink href={`/application-list/${studyId}`}>지원서 리스트 보기</StudyCardLink>
+    </Link>
   );
 }
