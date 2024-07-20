5da36d8aff59c62dfc8bd103e7b9913c91fc7b36
imkrmin <zum2n2@gmail.com>
2024-07-20 11:32:50 +0900

    Feat: 강의 카드에 카테고리 추가

--- src/components/domains/recruit/selectLecture/LectureCard.tsx
+++ src/components/domains/recruit/selectLecture/LectureCard.tsx
@@ -1,4 +1,6 @@
+import { STUDY_CATEGORY_MENU } from "@/constant/study";
 import { TLectureListCardData } from "@/types/api/lecture";
+import { StudyCategoryMenu } from "@/types/study";
 
 interface LectureCardProps {
   lecture: TLectureListCardData;
@@ -17,12 +19,19 @@ export default function LectureCard({ lecture, isSelected, setIsLecture }: Lectu
 
   return (
     <div
-      className={`w-full px-5 py-6 bg-white rounded-lg border flex justify-between items-start ${
+      className={`w-full px-4 py-6 bg-white rounded-lg border flex justify-between items-start ${
         isSelected ? "border-main-500" : "border-neutral-200"
       }`}>
       <button type="button" onClick={handleCardClick} className="flex w-full">
-        <div className="flex-col justify-start items-start gap-1 inline-flex">
-          <div className="text-sm font-semibold text-left leading-snug">{lecture.title}</div>
+        <div className="flex flex-col gap-1 w-full">
+          <div className="flex items-center gap-2 w-full">
+            <p className="inline-flex items-center py-[2px] px-[6px] bg-main-500 text-white text-xs font-semibold rounded-md leading-snug whitespace-nowrap">
+              {STUDY_CATEGORY_MENU[lecture.category as keyof StudyCategoryMenu].alt}
+            </p>
+            <p className="text-sm font-semibold text-left leading-snug truncate overflow-hidden whitespace-nowrap flex-grow">
+              {lecture.title}
+            </p>
+          </div>
           <p className="text-[12px] text-gray-600 text-left font-semibold leading-snug">{lecture.instructor}</p>
         </div>
       </button>
