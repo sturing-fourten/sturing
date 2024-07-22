6fea3cb5785cb051f1092a201d25a34beabd42b5
imkrmin <zum2n2@gmail.com>
2024-07-22 17:45:58 +0900

    Fix: 스터디 제목, 소개 글씨 제한 수정

--- src/components/domains/recruit/studyContent/StudyTitle.tsx
+++ src/components/domains/recruit/studyContent/StudyTitle.tsx
@@ -1,25 +1,34 @@
+import React, { useState } from "react";
+
 interface StudyTitleProps {
   studyTitle: string;
   handleTitleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export default function StudyTitle({ studyTitle, handleTitleChange }: StudyTitleProps) {
-  const minLength = 14;
-  const isError = studyTitle.length < minLength;
+  const minLength = 5;
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
+  const isError = isFocused && studyTitle.length < minLength;
 
   return (
     <div className="flex-col gap-2 inline-flex">
       <input
         type="text"
         name="study_title"
-        className={`w-full border py-3 px-4 flex items-center gap-2 rounded-[5px] focus:outline-none placeholder:text-[14px] placeholder:font-semibold placeholder:tracking-[-0.42px] placeholder:leading-[22px] ${
-          isError ? "border-red-500 focus:border-red-500" : "border-gray-300 focus:border-main-600"
-        }  text-[14px] font-semibold tracking-[-0.42px] leading-[22px] bg-white`}
+        className={`w-full border py-3 px-4 flex items-center gap-2 rounded-[5px] focus:outline-none placeholder:text-[14px] border-gray-300 placeholder:font-semibold placeholder:tracking-[-0.42px] placeholder:leading-[22px] ${
+          isError ? "focus:border-red-500" : "focus:border-main-600"
+        } text-[14px] font-semibold tracking-[-0.42px] leading-[22px] bg-white`}
         value={studyTitle}
-        placeholder="내 스터디를 돋보이게 하는 한마디 (최소 14자 이상 필수)"
+        placeholder="내 스터디를 돋보이게 하는 한마디 (최소 5자 이상 필수)"
         minLength={minLength}
         maxLength={24}
         onChange={handleTitleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
       />
       <div className="flex w-full text-zinc-800 text-xs font-normal mt-1">
         <div className="flex-grow">
