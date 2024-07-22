023a209a9cca0c09cf4933f8a99c0fd75851a56f
Hayden <hayden.dev.k@gmail.com>
2024-07-22 15:42:25 +0900

    Feat: 유저 인증 여부에 따라 페이지 접근 제한

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,25 @@
-import { NextRequest, NextResponse } from "next/server";
+import { auth as middleware } from "@/auth";
+import { NextResponse } from "next/server";
+
+const onlyLoggedInRoute = ["/mystudy", "/mypage", "/edit-profile", "/matching", "/recruit"];
+const routePattern = new RegExp(`^(${onlyLoggedInRoute.join("|")})`);
+
+export default middleware((req) => {
+  const { pathname } = req.nextUrl;
+  const isOnlyLoggedInRoute = routePattern.test(pathname);
+  if (!req.auth && isOnlyLoggedInRoute) {
+    return Response.redirect(new URL("/", req.nextUrl));
+  } else {
+    const requestHeaders = new Headers(req.headers);
+    requestHeaders.set("x-pathname", req.nextUrl.pathname);
+    return NextResponse.next({
+      request: {
+        headers: requestHeaders,
+      },
+    });
+  }
+});
+
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
-export async function middleware(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers);
-  requestHeaders.set("x-pathname", request.nextUrl.pathname);
-  return NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
-}
