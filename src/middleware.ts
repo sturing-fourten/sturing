b658e350f01594d85379c40c2f0d1dd647ff1bfa
Hayden <hayden.dev.k@gmail.com>
2024-07-22 16:44:37 +0900

    Feat: 로그인 후 홈으로 리다이렉션

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,18 @@ import { auth as middleware } from "@/auth";
 import { NextResponse } from "next/server";
 
 const onlyLoggedInRoute = ["/mystudy", "/mypage", "/edit-profile", "/matching", "/recruit", "/apply"];
-const routePattern = new RegExp(`^(${onlyLoggedInRoute.join("|")})`);
+const onlyLoggedOutRoute = ["/login"];
+const onlyLoggedInRoutePattern = new RegExp(`^(${onlyLoggedInRoute.join("|")})`);
+const onlyLoggedOutRoutePattern = new RegExp(`^(${onlyLoggedOutRoute.join("|")})`);
 
 export default middleware((req) => {
   const { pathname } = req.nextUrl;
-  const isOnlyLoggedInRoute = routePattern.test(pathname);
+  const isOnlyLoggedInRoute = onlyLoggedInRoutePattern.test(pathname);
+  const isOnlyLoggedOutRoute = onlyLoggedOutRoutePattern.test(pathname);
   if (!req.auth && isOnlyLoggedInRoute) {
     return Response.redirect(new URL("/login", req.nextUrl));
+  } else if (req.auth && isOnlyLoggedOutRoute) {
+    return Response.redirect(new URL("/", req.nextUrl));
   } else {
     const requestHeaders = new Headers(req.headers);
     requestHeaders.set("x-pathname", req.nextUrl.pathname);
