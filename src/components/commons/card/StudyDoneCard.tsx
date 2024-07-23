b9b88ce0e458ddea0442b8f530996853d88ff9b4
imkrmin <zum2n2@gmail.com>
2024-07-23 23:55:16 +0900

    Fix: 내 스터디 페이지 완료탭 하이드레이션 오류 해결

--- src/components/commons/card/StudyDoneCard.tsx
+++ src/components/commons/card/StudyDoneCard.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
 import StudyMeetingInfo from "./element/StudyMeetingInfo";
-import StudyCardLink from "./element/StudyCardLink";
-import { TMember, TMemberUserIdAddedUser, TMyStudy, TTeamMembersIdAddedMember } from "@/types/study";
+import { TMyStudy, TTeamMembersIdAddedMember } from "@/types/study";
 import { getDateRange } from "@/utils/getDateRange";
 import { getSession } from "@/lib/database/getSession";
 import LinkCardWrapper from "@/components/domains/mystudy/LinkCardWrapper";
-import LinkButtonWrapper from "@/components/domains/mystudy/LinkButtonWrapper";
+import { TeamMemberReviewItem } from "./element/TeamMemberReviewItem";
+import LectureReviewButton from "./element/LectureReviewButton";
+import ReceivedReviewButton from "./element/ReceivedReviewButton";
 
 interface IStudyDoneCardProps {
   study: TMyStudy;
@@ -33,6 +33,7 @@ export default async function StudyDoneCard(props: IStudyDoneCardProps) {
 
   const dateRange = getDateRange(startDate, endDate);
   const where = (format === "ONLINE" ? platform : location) ?? "";
+  const lectureIdString = lectureId.toString();
 
   return (
     <LinkCardWrapper
@@ -41,39 +42,13 @@ export default async function StudyDoneCard(props: IStudyDoneCardProps) {
       <div className="w-full text-left">
         <StudyMeetingInfo format={"ONLINE" ? "온라인" : "오프라인"} dateRange={dateRange} where={where} />
         <p className="mt-2 mb-3 text-[#212121] text-[16px] font-semibold tracking-[-0.32px]">{title}</p>
-        <StudyCardLink href={`/lecture/${lectureId}/review`}>강의 후기 작성하기</StudyCardLink>
+        <LectureReviewButton lectureId={lectureIdString}>강의 후기 작성하기</LectureReviewButton>
       </div>
       <hr className="bg-gray-300" />
       {teamMemberList.map((member) => (
         <TeamMemberReviewItem key={member.userId?._id.toString()} member={member} />
       ))}
-      <StudyCardLink href="">내가 받은 후기 보기</StudyCardLink>
+      <ReceivedReviewButton>내가 받은 후기 보기</ReceivedReviewButton>
     </LinkCardWrapper>
-  );
-}
-
-function TeamMemberReviewItem({ member }: { member: TMember }) {
-  /**
-   * @todo review 유무 연동
-   */
-
-  const review = false;
-  const user = member.userId as TMemberUserIdAddedUser;
-  return (
-    <div className="flex items-center justify-between w-full">
-      <p className="text-[#212121] text-[14px] font-semibold tracking-[-0.42px]">{user.nickname}</p>
-
-      {review ? (
-        <button className="py-[6px] px-3 rounded-[5px] border border-gray-400 text-gray-700 text-[12px] font-medium tracking-[-0.36px]">
-          작성 완료
-        </button>
-      ) : (
-        <Link
-          className="py-[6px] px-3 rounded-[5px] border border-main-500 text-main-500 text-[12px] font-medium tracking-[-0.36px]"
-          href={`/member-review/${member._id}`}>
-          팀원 후기 작성하기
-        </Link>
-      )}
-    </div>
   );
 }
