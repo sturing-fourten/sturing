88c1b0131892482c77c672c6fc38cb29f5940446
imkrmin <zum2n2@gmail.com>
2024-07-23 17:45:39 +0900

    Fix: 대기탭 모집 중 카드에 제한없음 수정

--- src/components/commons/card/element/StudyDetailInfo.tsx
+++ src/components/commons/card/element/StudyDetailInfo.tsx
@@ -9,12 +9,13 @@ interface IStudyDetailInfoProps {
 }
 export function StudyDetailInfo(props: IStudyDetailInfoProps) {
   const { className, meetingDay, memberCount, task } = props;
+  console.log(memberCount);
   return (
     <section
       className={`flex justify-evenly items-center p-2 rounded-[3px] bg-gray-100 text-gray-1000 text-[14px] font-medium tracking-[-0.42px] ${className}`}>
       <div className="flex gap-1">
         <Image src={teammate} alt="teammate icon" width={18} height={18} />
-        팀원 {memberCount}명
+        팀원 {memberCount}
       </div>
       <span className="w-[1px] h-3 bg-gray-400"></span>
       <div className="flex gap-1">
