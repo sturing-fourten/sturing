d5205e6134007b07fb9bf1662a200d52a2e5265c
Hayden <hayden.dev.k@gmail.com>
2024-07-24 09:48:29 +0900

    Fix: 선택한 날짜나 미팅 일정이 없을 경우 에러 처리

--- src/utils/getIsMeetingDay.ts
+++ src/utils/getIsMeetingDay.ts
@@ -1,6 +1,7 @@
 import { startOfDay } from "date-fns";
 
 export const getIsMeetingDay = (meetingList: Date[], currentDate: Date): boolean => {
+  if (!meetingList || !currentDate) return false;
   const targetDate = startOfDay(currentDate);
 
   const index = meetingList.findIndex((meeting) => startOfDay(meeting).getTime() === targetDate.getTime());
@@ -9,6 +10,7 @@ export const getIsMeetingDay = (meetingList: Date[], currentDate: Date): boolean
 };
 
 export const getMeetingDayCount = (meetingList: Date[], currentDate: Date): number => {
+  if (!meetingList || !currentDate) return -1;
   const targetDate = startOfDay(currentDate);
 
   const index = meetingList.findIndex((meeting) => startOfDay(meeting).getTime() === targetDate.getTime());
