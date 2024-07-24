af62a5121e5cce3aeed86b7b51a012d0935ad707
imkrmin <zum2n2@gmail.com>
2024-07-24 15:29:20 +0900

    Feat: 마이페이지 강의 scrap api 연결

--- src/app/(userProfile)/mypage/scrap/@tabs/lecture/page.tsx
+++ src/app/(userProfile)/mypage/scrap/@tabs/lecture/page.tsx
@@ -1,23 +1,10 @@
 import LectureCard from "@/components/commons/card/LectureCard";
-import { getBookmarkListAction } from "@/lib/database/action/bookmarkList";
+import { getLectureBookmarkListAction } from "@/lib/database/action/bookmarkList";
 import { TLectureInfoData } from "@/types/api/lecture";
 import Link from "next/link";
 
-const fetchLectureData = async (lectureId: string) => {
-  const response = await fetch(`${process.env.LOCAL_URL}/api/lecture/${lectureId}`);
-
-  if (!response.ok) {
-    throw new Error("강의 불러오기 실패");
-  }
-
-  const data = await response.json();
-  return data.lecture;
-};
-
 export default async function ScrapLecturePage() {
-  const lectureBookmarkList = await getBookmarkListAction("lecture");
-  const lectureDataPromises = lectureBookmarkList.map((bookmark: any) => fetchLectureData(bookmark.lectureId));
-  const lectureBookmarkData = await Promise.all(lectureDataPromises);
+  const lectureBookmarkData = await getLectureBookmarkListAction();
 
   return (
     <ul className="flex flex-col gap-4 py-5 px-4">
