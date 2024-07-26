61f4ce0e97f2c832974f24ca1af682701df1eaa9
imkrmin <zum2n2@gmail.com>
2024-07-26 19:00:33 +0900

    Fix: 관심목록 페이지 북마크 된 스터디 이동 시 상세페이지 에러 수정

--- src/app/(userProfile)/mypage/scrap/@tabs/page.tsx
+++ src/app/(userProfile)/mypage/scrap/@tabs/page.tsx
@@ -4,11 +4,12 @@ import Link from "next/link";
 
 export default async function ScrapStudyPage() {
   const studyBookmarkList = await getStudyBookmarkListAction();
+  console.log(studyBookmarkList);
 
   return (
     <ul className="grid grid-cols-2 gap-2 py-5 px-4">
       {studyBookmarkList.map((study: any) => (
-        <Link href={`/study/${study._id}`} key={study._id}>
+        <Link href={`/study/${study.id}`} key={study.id}>
           <StudyRecruitCard isMini={true} recruitCardData={study} />
         </Link>
       ))}
