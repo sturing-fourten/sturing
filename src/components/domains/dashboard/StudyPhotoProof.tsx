ed95530f80df9c99cac023dafa94fccea4aa9420
Hayden <hayden.dev.k@gmail.com>
2024-07-26 22:06:45 +0900

    Fix: 아직 사용하지 않는 사진 인증 컴포넌트 에러 유발 코드 수정

--- src/components/domains/dashboard/StudyPhotoProof.tsx
+++ src/components/domains/dashboard/StudyPhotoProof.tsx
@@ -6,7 +6,7 @@ import WriteBoardLink from "./WriteBoardLink";
 export default function StudyPhotoProof() {
   return (
     <DashboardCardPaginationLayout hasMore={true}>
-      <DashboardCardTitle isEditing={true} title="사진 인증" />
+      <DashboardCardTitle title="사진 인증" />
 
       <ul className="grid grid-cols-3 gap-4 justify-between">
         {[1, 2, 3, 4, 5, 6].map((photo, index) => (
