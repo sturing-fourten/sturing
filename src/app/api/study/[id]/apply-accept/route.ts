986317c2e1fd2b5d54a484977556fa228e097b4f
Hayden <hayden.dev.k@gmail.com>
2024-07-28 15:56:55 +0900

    Feat: 지원 수락 API 구현 및 UI 연동

--- src/app/api/study/[id]/apply-accept/route.ts
+++ src/app/api/study/[id]/apply-accept/route.ts
@@ -1,11 +1,11 @@
 import connectDB from "@/lib/database/db";
 import { Study } from "@/schema/studySchema";
+import { TeamMembers } from "@/schema/teamMemberSchema";
 import { User } from "@/schema/userSchema";
 
 export async function PATCH(req: Request, { params }: { params: { id: string } }) {
   const studyId = params.id;
-  const { applicantId } = await req.json();
-
+  const { appliedUserId } = await req.json();
   const token = req.headers.get("Authorization");
   let userId = token?.split(" ")[1];
   await connectDB();
@@ -24,6 +24,7 @@ export async function PATCH(req: Request, { params }: { params: { id: string } }
       return Response.json({ message: "스터디가 존재하지 않습니다" }, { status: 404 });
     }
 
+    // 1. 권한 체크
     const currentStudyOwnerId = currentStudy.ownerId.toString();
     const user = await User.findById(userId);
 
@@ -35,7 +36,17 @@ export async function PATCH(req: Request, { params }: { params: { id: string } }
       return Response.json({ message: "권한이 없습니다" }, { status: 403 });
     }
 
-    //구현 예정
+    // 2. 팀멤버에서 상태를 accepted로
+    const updatedTeamMember = await TeamMembers.findOneAndUpdate(
+      { studyId, "members.userId": appliedUserId },
+      { $set: { "members.$.status": "ACCEPTED" } },
+      { new: true },
+    );
+
+    if (!updatedTeamMember) {
+      return new Response(JSON.stringify({ message: "해당 조건의 팀 멤버를 찾을 수 없습니다" }), { status: 404 });
+    }
+    return new Response(JSON.stringify({ message: "팀 멤버 상태가 업데이트 되었습니다" }), { status: 200 });
   } catch (error: any) {
     if (error.name === "CastError") {
       return Response.json({ message: "요청을 처리할 수 없습니다." }, { status: 404 });
