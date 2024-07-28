2a02cedd9f3d4bd986002a9fdaed4b92998f9b25
Joohyun Kim <trust987@naver.com>
2024-07-28 19:10:28 +0900

    Feat: 스터디 완료 api 스터디 완료시 팀멤버 스터링 지수 올려 주도록 수정

--- src/app/api/my-study/[id]/done/route.ts
+++ src/app/api/my-study/[id]/done/route.ts
@@ -1,11 +1,13 @@
 import connectDB from "@/lib/database/db";
 import { Study } from "@/schema/studySchema";
+import { TeamMembers } from "@/schema/teamMemberSchema";
 import { User } from "@/schema/userSchema";
+import { Types } from "mongoose";
 
 export async function PATCH(req: Request, { params }: { params: { id: string } }) {
   const studyId = params.id;
-  const { userId } = await req.json();
-  connectDB();
+  const token = req.headers.get("Authorization");
+  const userId = token?.split(" ")[1];
 
   if (!studyId) {
     return Response.json({ error: "스터디 id 가 필요합니다." }, { status: 400 });
@@ -13,8 +15,8 @@ export async function PATCH(req: Request, { params }: { params: { id: string } }
   if (!userId) {
     return Response.json({ error: "유저 id 가 필요합니다." }, { status: 400 });
   }
-  connectDB();
   try {
+    await connectDB();
     const currentStudy = await Study.findById(studyId);
 
     if (!currentStudy) {
@@ -25,7 +27,7 @@ export async function PATCH(req: Request, { params }: { params: { id: string } }
     const user = await User.findById(userId);
 
     if (!user) {
-      return Response.json({ message: "유저가 존재하지 않습니다" }, { status: 404 });
+      return Response.json({ message: "존재하지 않는 유저입니다" }, { status: 404 });
     }
 
     if (currentStudyOwnerId !== user.id) {
@@ -38,14 +40,22 @@ export async function PATCH(req: Request, { params }: { params: { id: string } }
       return Response.json({ message: "이미 완료된 스터디입니다." }, { status: 400 });
     }
 
+    const teamMembers = await TeamMembers.findOne({ studyId });
+    const acceptedTeamMembers = teamMembers.members.filter(({ status }: { status: string }) => {
+      return status === "ACCEPTED";
+    });
+    await Promise.all(
+      acceptedTeamMembers.map(async ({ userId }: { userId: Types.ObjectId }) => {
+        await User.updateOne({ _id: userId, sturingIndex: { $lt: 96 } }, { $inc: { sturingIndex: 5 } });
+      }),
+    );
     await Study.updateOne({ _id: studyId }, { status: "DONE" });
-
     return Response.json({ message: "스터디가 완료 되었습니다." }, { status: 200 });
   } catch (error: any) {
     if (error.name === "CastError") {
       return Response.json({ message: "요청을 처리할 수 없습니다." }, { status: 404 });
     } else {
-      return Response.json({ message: error.name }, { status: 500 });
+      return Response.json({ error }, { status: 500 });
     }
   }
 }
