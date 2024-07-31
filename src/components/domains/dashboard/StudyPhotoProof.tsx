0bb2ac03001677623f89549113f2b25a06bdb28d
Hayden <hayden.dev.k@gmail.com>
2024-07-31 10:41:10 +0900

    Fix: 빌드 에러 유발하는 개발 대상 외 컴포넌트 주석 처리

--- src/components/domains/dashboard/StudyPhotoProof.tsx
+++ src/components/domains/dashboard/StudyPhotoProof.tsx
@@ -5,14 +5,16 @@ import WriteBoardLink from "./WriteBoardLink";
 
 export default function StudyPhotoProof() {
   return (
-    <DashboardCardPaginationLayout hasMore={true}>
-      <DashboardCardTitle title="사진 인증" />
+    <></>
+    // @todo 사진 인증 추가 개발 예정
+    // <DashboardCardPaginationLayout hasMore={true}>
+    //   <DashboardCardTitle title="사진 인증" />
 
-      <ul className="grid grid-cols-3 gap-4 justify-between">
-        {[1, 2, 3, 4, 5, 6].map((photo, index) => (
-          <PhotoProofThumbnail key={index} />
-        ))}
-      </ul>
-    </DashboardCardPaginationLayout>
+    //   <ul className="grid grid-cols-3 gap-4 justify-between">
+    //     {[1, 2, 3, 4, 5, 6].map((photo, index) => (
+    //       <PhotoProofThumbnail key={index} />
+    //     ))}
+    //   </ul>
+    // </DashboardCardPaginationLayout>
   );
 }
