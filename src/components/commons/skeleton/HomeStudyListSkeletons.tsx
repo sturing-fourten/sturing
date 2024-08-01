c67cc72cc069ff6b47a7a9496f7f434f30ba407a
Joohyun Kim <148737398+kimbobby@users.noreply.github.com>
2024-08-01 20:34:32 +0900

    Feat: 검색 결과 페이지 스켈레톤 (#173)

--- src/components/commons/skeleton/HomeStudyListSkeletons.tsx
+++ src/components/commons/skeleton/HomeStudyListSkeletons.tsx
@@ -4,7 +4,7 @@ import StudyRecruitCardSkeleton from "./StudyRecruitCardSkeleton";
 export default function HomeStudyListSkeletons() {
   return (
     <>
-      <div className=" animate-pulse h-5 w-[140px] bg-gray-300 rounded-[3px]" />
+      <div className=" animate-pulse h-5 w-[140px] bg-gray-200 rounded-[3px]" />
       <CardList isSingleLine>
         {new Array(6).fill("").map((_, i) => (
           <StudyRecruitCardSkeleton key={i} />
